bcfc8fe130b8a4c1e14b08a2dfb7f69623e4fad8
Nguyễn Đặng Tuấn Kiệt <87163945+AmoryBunzz@users.noreply.github.com>
2021-12-08 16:42:59 +0700

    login + register form submit event handler

--- python_final/templates/auth/login.js
+++ python_final/templates/auth/login.js
@@ -1,3 +1,4 @@
+const loginForm = document.getElementById("loginForm");
 const forgotPasswordForm = document.getElementById("forgotPasswordForm");
 
 function showForgotPassword() {
@@ -7,3 +8,25 @@ function showForgotPassword() {
 function closeForgotPassword() {
     forgotPasswordForm.classList.add("hidden");
 }
+
+loginForm.addEventListener("submit", e => {
+    e.preventDefault();
+    
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+    
+    // TODO: send axios login request with email and password
+    axios.post("...", {email: email, password: password}).then((res) => {
+        if (res.data.message === 'Authentication successful!'){
+            localStorage.setItem('token', res.data.token);
+            axios.defaults.headers.common['Authorization'] = res.data.token;
+            parent.location='...' // Link to home page
+        }
+        else {
+            document.getElementById("password").value = ""
+            alert(res.data.message);
+        }
+    }).catch(e => {
+        console.log(e);
+    })
+})
